a13ddf1ceedea5c429df2e06d9f6bd74282b39e5
NafeesIS <nafeesshawon555@gmail.com>
2025-08-30 22:49:02 +0600

    created shiritori game

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,124 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Trophy, Users } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { toast, Toaster } from "sonner";
 
-export default function Home() {
+const HomePage = () => {
+  const [inputWord, setInputWord] = useState("");
+  const [historyWord, setHistoryWord] = useState<string[]>([]);
+  const [timer, setTimer] = useState(10);
+  const [playerTurn, setPlayerTurn] = useState<1 | 2>(1);
+  const [score, setScore] = useState({ p1: 0, p2: 0 });
+
+  // countdown timer logic set to 10 seconds interval
+  useEffect(() => {
+    if (timer === 0) {
+      setScore((prev) => ({
+        ...prev,
+        [playerTurn === 1 ? "p1" : "p2"]:
+          prev[playerTurn === 1 ? "p1" : "p2"] - 1,
+      }));
+      setPlayerTurn((prev) => (prev === 1 ? 2 : 1));
+      setTimer(10);
+      toast.warning("Time's Up!");
+    }
+    const countDown = setInterval(() => setTimer((t) => t - 1), 1000); // 10 seconds
+    return () => clearInterval(countDown);
+  }, [timer]);
+
+  // validate any english word by api
+  const validateWord = async () => {
+    const word = inputWord.toLocaleLowerCase().trim();
+
+    if (word.length < 4) return toast.error("Word must be at least 4 letters!");
+    if (historyWord.includes(word))
+      return toast.error("This word is already used try another one!");
+
+    try {
+      const res = await fetch(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+      );
+      if (!res.ok) {
+        setScore((prev) => ({
+          ...prev,
+          [playerTurn === 1 ? "p1" : "p2"]:
+            prev[playerTurn === 1 ? "p1" : "p2"] - 1,
+        }));
+        return toast.error("Not a valid English Word!");
+      }
+      toast.success("Valid Word!");
+      setScore((prev) => ({
+        ...prev,
+        [playerTurn === 1 ? "p1" : "p2"]:
+          prev[playerTurn === 1 ? "p1" : "p2"] + 1,
+      }));
+      setPlayerTurn((prev) => (prev === 1 ? 2 : 1));
+      setTimer(10);
+      setInputWord("");
+      setHistoryWord((prev) => [...prev, word]);
+    } catch (e) {
+      toast.error("Failed to validate");
+    }
+  };
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="min-h-screen bg-gradient-to-br from blue-50 to-indigo-100 p-4 lg:p-6">
+      <div className="max-w-3xl mx-auto">
+        <div className="text-center">
+          <h1 className="font-bold text-3xl flex justify-center items-center gap-2">
+            <Users className="size-5" />
+            <span>Shiritori Game</span>
+          </h1>
+          <p>Connect the words by first and last latter!</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div>
+          <h2 className="font-semibold text-xl flex justify-center items-center gap-2 my-4">
+            <Trophy className="size-5" /> Scores
+          </h2>
+          <div className="w-full grid grid-cols-2 text-center text-lg font-semibold gap-4">
+            <div className="bg-white border rounded-lg space-y-2 p-2">
+              <p>Player 1</p> <p>{score.p1}</p>
+            </div>
+            <div className="bg-white border rounded-lg space-y-2 p-2">
+              <p>Player 2</p> <p>{score.p2}</p>
+            </div>
+          </div>
+          <div>
+            <div className="justify-center font-semibold text-xl mt-4 flex items-center gap-2">
+              <h3>Player {playerTurn}'s Turn</h3>{" "}
+              <p className="text-red-600">({timer} seconds)</p>
+            </div>
+            <div className="text-center font-medium mt-2 text-xs lg:text-sm">
+              Last Word {historyWord[historyWord.length - 1]}
+            </div>
+            <div className="mt-4 flex items-center">
+              <Input
+                type="text"
+                value={inputWord}
+                onChange={(e) => setInputWord(e.target.value)}
+                placeholder="Enter Your Word"
+                className="bg-white rounded-r-none h-8 focus:outline-none focus:ring-0 ring-0 outline-0"
+              />
+              <Button className="h-8 rounded-l-none" onClick={validateWord}>
+                Submit
+              </Button>
+            </div>
+            <div className="mx-auto max-w-80 text-center">
+              <h6 className="text-lg font-bold mt-6 mb-2">Word History</h6>
+              <div className="text-center h-96 overflow-scroll bg-white p-6 ">
+                {historyWord.map((history) => (
+                  <p>{history}</p>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Toaster position="top-center" closeButton richColors />
+    </main>
   );
-}
+};
+
+export default HomePage;
